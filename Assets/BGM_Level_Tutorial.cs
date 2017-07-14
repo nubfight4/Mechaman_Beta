00d21721bfa68ed0eee3b347bbc6a7c399aac947
Cks <pikacusheng@gmail.com>
2017-07-14 14:48:17 +0800

    taking sound manager

---
+++ Assets/BGM_Level_Tutorial.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM_Level_Tutorial : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+        SoundManagerScript.Instance.PlayBGM(AudioClipID.BGM_TUTORIAL);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/SCRIPTS/BGM_Initiator_Level_1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM_Initiator_Level_1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SoundManagerScript.Instance.PlayBGM(AudioClipID.BGM_LEVEL_1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
