e7c03c40c237af75e312e080cf4e2f80096d527c
Ooi Yee Soon <0111509@kdu-online.com>
2017-07-18 16:06:01 +0800

    Title Menu Update into our Vertical Slice build. Script n animations running clear~ Help check if got any problem,

---
+++ Assets/Title_Menu/titleMenuScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class titleMenuScript : MonoBehaviour
+{
+    public Animator anim;
+    bool walkAnimBool, punchAnimBool;
+    public int playerNo;
+        
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        walkAnimBool = false;
+        punchAnimBool = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerNo == 1)
+        {
+            if (Input.GetButton("Bumper_Left_P1"))
+            {
+                Debug.Log("Player 1 holds the button~");
+                walkAnimBool = true;
+            }
+            else
+            {
+                walkAnimBool = false;
+            }
+        }
+        if (playerNo == 2)
+        {
+            if (Input.GetButton("Bumper_Right_P2"))
+            {
+                Debug.Log("Player 2 holds the button~");
+                punchAnimBool = true;
+            }
+            else
+            {
+                punchAnimBool = false;
+            }
+        }
+
+        if (Input.GetButton("Bumper_Left_P1") && Input.GetButton("Bumper_Right_P2"))
+        {
+            SceneManager.LoadScene("MainMenuScene");
+        }
+        UpdateAnim();
+    }
+
+    void UpdateAnim()
+    {
+        anim.SetBool("punchAnimBool", punchAnimBool);
+        anim.SetBool("WalkAnimBool", walkAnimBool);
+    }
+}
